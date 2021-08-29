423150e45eecb654c6d66c59000a7f60af2a2297
Masato0816 <77476667+Masato0816@users.noreply.github.com>
2021-08-29 23:54:40 +0900

    変更済み

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,24 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  const full_name = data.family_name + ' ' + data.first_name
+  data["full_name"] = full_name;
+  return data
+}
+
+function test(data) {
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      const result = data.map(buildFullName);
+      resolve(result);
+    }, 3000);
+  });
+
+  return promise
 }
 
